cee343172c637c837e5f5d14e262a18e788cde17
David Zimberknopf <dudi@vizo.dev>
2020-10-12 18:11:25 +0300

    'commit'

--- lib/constants/constants.d.ts
+++ lib/constants/constants.d.ts
@@ -1,2 +1,3 @@
 export declare const ADMIN_HOST_AVATAR =
   'https://cdn0.iconfinder.com/data/icons/man-user-human-profile-avatar-person-business/100/10B-1User-512.png';
+export declare const DEFAULT_USER_IMAGE_BUCKET = 'meetplace-auth-faces';
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,4 @@
 export const ADMIN_HOST_AVATAR =
   'https://cdn0.iconfinder.com/data/icons/man-user-human-profile-avatar-person-business/100/10B-1User-512.png';
+
+export const DEFAULT_USER_IMAGE_BUCKET = 'meetplace-auth-faces';
